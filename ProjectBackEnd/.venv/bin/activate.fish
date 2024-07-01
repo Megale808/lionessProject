4ac8e61ec66cc330f97a10152ced9ca12722e74e
Roger lee <rogerlee955@gmail.com>
2024-07-01 15:02:18 +0300

    code update 2

--- ProjectBackEnd/.venv/bin/activate.fish
+++ ProjectBackEnd/.venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/rogerlee808/whiskey/assessments/lioness_project/.venv"
+set -gx VIRTUAL_ENV "/home/rogerlee808/whiskey/assessments/lionessProject/ProjectBackEnd/.venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
