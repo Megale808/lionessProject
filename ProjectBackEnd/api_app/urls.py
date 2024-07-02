3a7f43f4860e0cd0735932531fe873d734cd9fbc
Roger lee <rogerlee955@gmail.com>
2024-07-02 14:39:15 +0300

    code review and clean up

--- ProjectBackEnd/api_app/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import Noun_Project, Page_Noun_Project
-
-urlpatterns = [
-    path('', Noun_Project.as_view(), name="noun_project"),
-    path('<str:name>/', Page_Noun_Project.as_view(), name="page_noun_project"),
-]
